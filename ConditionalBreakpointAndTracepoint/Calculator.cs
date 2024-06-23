dadc8ce87300fbbdd068e9bf3b866432db8e66c5
Emrullah IŞIK <emrullah04@outlook.com>
2024-06-23 21:44:42 +0300

    Add project files.

---
+++ ConditionalBreakpointAndTracepoint/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace ConditionalBreakpointAndTracepoint;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ ConditionalBreakpointAndTracepoint/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using ConditionalBreakpointAndTracepoint;
+
+Calculator calculator = new Calculator();
+for (int i = 0; i < 10; i++)
+{
+    int result = calculator.Add(i, 5);
+    Console.WriteLine($"Iteration: {i}, Result: {result}");
+    if (result == 10)
+    {
+        Console.WriteLine("Result is 10!");
+    }
+}
